4127c21668380368230bd9a9392f1248a205bf0a
sourav-mahmud <souravmahmud32@gmail.com>
2024-08-28 13:43:51 +0600

    updated

--- server.js
+++ server.js
@@ -9,14 +9,7 @@ const app = express();
 
 const path = require('path');
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 
 // Use CORS middleware
